fc1733d715dc8dcb6eb406ac188740b9a59bc0e0
demo-program <aleksandra.charuba@gmail.com>
2016-08-01 15:05:15 +0200

    dataBase

---
+++ ConsoleApplication1/ApplicationDB/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ApplicationDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Add = new System.Windows.Forms.Button();
+            this.A = new System.Windows.Forms.Label();
+            this.B = new System.Windows.Forms.Label();
+            this.suma = new System.Windows.Forms.Label();
+            this.roznica = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(77, 115);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 0;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A
+            // 
+            this.A.AutoSize = true;
+            this.A.Location = new System.Drawing.Point(18, 22);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(14, 13);
+            this.A.TabIndex = 1;
+            this.A.Text = "A";
+            this.A.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // B
+            // 
+            this.B.AutoSize = true;
+            this.B.Location = new System.Drawing.Point(18, 45);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(14, 13);
+            this.B.TabIndex = 2;
+            this.B.Text = "B";
+            // 
+            // suma
+            // 
+            this.suma.AutoSize = true;
+            this.suma.Location = new System.Drawing.Point(18, 67);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(32, 13);
+            this.suma.TabIndex = 3;
+            this.suma.Text = "suma";
+            // 
+            // roznica
+            // 
+            this.roznica.AutoSize = true;
+            this.roznica.Location = new System.Drawing.Point(18, 94);
+            this.roznica.Name = "roznica";
+            this.roznica.Size = new System.Drawing.Size(41, 13);
+            this.roznica.TabIndex = 4;
+            this.roznica.Text = "roznica";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(65, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(65, 91);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.roznica);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.Add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Label A;
+        private System.Windows.Forms.Label B;
+        private System.Windows.Forms.Label suma;
+        private System.Windows.Forms.Label roznica;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
